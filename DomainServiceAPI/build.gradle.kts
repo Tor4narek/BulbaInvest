12c831de1ee4e35e739dc64b420deca880c9c374
Oleg <haitsynoleg@yandex.by>
2026-05-20 00:02:09 +0300

    build: created full project dev stand

--- DomainServiceAPI/build.gradle.kts
+++ DomainServiceAPI/build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
     application
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "com.bulbainvest"
